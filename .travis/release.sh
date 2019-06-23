eff65a88c8b19d0ddefc3dac2cfc0e0faf9edca6
Andreas Neeb <aneeb@redhat.com>
2019-06-23 17:58:01 +0200

    splitting into stage and release deploys

--- .travis/release.sh
+++ .travis/release.sh
@@ -6,7 +6,7 @@ echo "Ensuring that pom  matches $TRAVIS_TAG"
 mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
 
 echo "Uploading to oss repo and GitHub"
-mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots
+mvn deploy -DskipTests=true --batch-mode --update-snapshots
 
 echo "Deploying to Stage"
 tower-cli config host $TOWER_URL
