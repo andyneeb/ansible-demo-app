7cae64089e32849992fed19f0867507a2c2dca54
Andreas Neeb <aneeb@redhat.com>
2019-06-23 19:16:05 +0200

    setting extra_var instance_name=all for prod release

--- .travis/release.sh
+++ .travis/release.sh
@@ -13,6 +13,6 @@ tower-cli config host $TOWER_URL
 tower-cli config verify_ssl false
 tower-cli config username $TOWER_USER
 tower-cli config password $TOWER_PASSWORD
-tower-cli job_template create --name "Release Demo App v$TRAVIS_BUILD_NUMBER" --project "Demo App Repo" --playbook "plays/simple.yml" --inventory "OpenStack Prod" --extra-vars=app_release=$TRAVIS_BUILD_NUMBER --extra-vars=app_stage=release --ask-limit-on-launch TRUE --credential "RHEL Cloud User Secure"
-tower-cli role grant --type "execute" --user "release_manager" --job-template "Release Demo App v$(lastversion andyneeb/ansible-demo-app)"
+tower-cli job_template create --name "Release Demo App v$TRAVIS_BUILD_NUMBER" --project "Demo App Repo" --playbook "plays/simple.yml" --inventory "OpenStack Prod" --extra-vars=app_release=$TRAVIS_BUILD_NUMBER --extra-vars=instance_name=all --ask-limit-on-launch TRUE --credential "RHEL Cloud User Secure"
+tower-cli role grant --type "execute" --user "release_manager" --job-template "Release Demo App v$TRAVIS_BUILD_NUMBER"
 tower-cli job_template delete --name "Release Demo App v$(bc <<< $TRAVIS_BUILD_NUMBER-2)"
