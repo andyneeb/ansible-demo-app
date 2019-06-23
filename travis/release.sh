c07237c5c4cb3fc6ea0077fcb63fb026b766a989
Andreas Neeb <aneeb@redhat.com>
2019-06-23 17:43:58 +0200

    new stab at a simpler travis pipe

---
+++ travis/release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Ensuring that pom  matches $TRAVIS_TAG"
+mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
+
+echo "Uploading to oss repo and GitHub"
+mvn deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots
+
+echo "Deploying to Stage"
+tower-cli config host $TOWER_URL
+tower-cli config verify_ssl false
+tower-cli config username $TOWER_USER
+tower-cli config password $TOWER_PASSWORD
