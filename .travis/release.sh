e5a847bae3042eebbf2c4e4df01c4b8f92d5dbc2
Andreas Neeb <aneeb@redhat.com>
2019-06-25 10:33:36 +0200

    implementing logic to rotate latest and latest-1

--- .travis/release.sh
+++ .travis/release.sh
@@ -2,6 +2,7 @@
 
 set -e
 export COMMIT=${TRAVIS_COMMIT::7}
+export CURRENT=$(tower-cli job_template get --name "Release Demo App (latest)" -f id)
 
 echo "Ensuring that pom matches $COMMIT"
 mvn org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$COMMIT
@@ -14,8 +15,15 @@ tower-cli config host $TOWER_URL
 tower-cli config verify_ssl false
 tower-cli config username $TOWER_USER
 tower-cli config password $TOWER_PASSWORD
-tower-cli job_template create --name "Release Demo App v$COMMIT" --project "Demo App Repo" --playbook "plays/simple.yml" --inventory "OpenStack Prod" --extra-vars=app_release="$COMMIT" --extra-vars=instance_name=all --ask-limit-on-launch TRUE --credential "RHEL Cloud User Secure"
-tower-cli job_template associate_notification_template --job-template "Release Demo App v$COMMIT" --notification-template 1 --status=error
-tower-cli job_template associate_notification_template --job-template "Release Demo App v$COMMIT" --notification-template 1 --status=success
-tower-cli role grant --type "execute" --user "release_manager" --job-template "Release Demo App v$COMMIT"
-#tower-cli job_template delete --name "Release Demo App v$(bc <<< $TRAVIS_BUILD_NUMBER-3)"
+
+echo "Delete N-1"
+tower-cli job_template delete --name "Release Demo App (latest-1)"
+
+echo "Rename Current to N-1"
+tower-cli job_template copy $CURRENT --new-name "Release Demo App (latest-1)"
+
+echo "Create new job for latest"
+tower-cli job_template create --name "Release Demo App (latest)" --project "Demo App Repo" --playbook "plays/simple.yml" --inventory "OpenStack Prod" --extra-vars=app_release="$COMMIT" --extra-vars=instance_name=all --ask-limit-on-launch TRUE --credential "RHEL Cloud User Secure"
+tower-cli job_template associate_notification_template --job-template "Release Demo App (latest)" --notification-template 1 --status=error
+tower-cli job_template associate_notification_template --job-template "Release Demo App (latest)" --notification-template 1 --status=success
+tower-cli role grant --type "execute" --user "release_manager" --job-template "Release Demo App (latest)"
